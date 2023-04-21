8cb0badd0d7cf172b03275f3adca83067d8fb534
JCGrant42 <kadenith@gmail.com>
2023-04-20 20:15:32 -0600

    Did the assignment for both Prep1 and Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letterGrade = "";
+
+        if (number >= 90){
+            letterGrade = "A";
+        }
+        else if(number >= 80){
+            letterGrade = "B";
+        }
+        else if(number >= 70){
+            letterGrade = "C";        
+        }
+        else if(number >= 60){
+            letterGrade = "D";           
+        }
+        else {
+            letterGrade = "F";    
+        }
+
+        string symbol = "";
+        if (number >= 60 && number <= 96){
+            int firstDigit = number % 10;
+            if (firstDigit >= 7){
+                symbol = "+";
+            }
+            else if(number < 3){
+                symbol = "-";
+            }
+        }
+
+        Console.WriteLine($"Your grade is {letterGrade}{symbol}.");
+             
+
+        if (number >= 70){
+            Console.WriteLine("congratulations, You Pass");
+        }
+        else {
+            Console.WriteLine("Your grade was not high enough, keep on trying");
+        }
+
+
     }
 }
