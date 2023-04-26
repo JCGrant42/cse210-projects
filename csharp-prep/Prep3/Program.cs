c5c210f98383e8498d197e7632b768e7ed3f2158
JCGrant42 <kadenith@gmail.com>
2023-04-25 21:45:24 -0600

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random RandomGenerator = new Random();
+        int RandumNum = RandomGenerator.Next(1,100);
+
+
+        while (true) {
+            Console.Write("Please enter a number ");
+            int guessNum = int.Parse(Console.ReadLine());
+
+
+            if (guessNum > RandumNum) {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum < RandumNum){
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("You guess correctly!!!");
+                break;
+            }
+        }
     }
 }
