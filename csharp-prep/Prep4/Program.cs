4b7e3cb38726429a6ff3c54bc631c7740658561d
JCGrant42 <kadenith@gmail.com>
2023-04-26 21:29:22 -0600

    Completed Prep 4 & 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        while (true) {
+            //get input
+            Console.Write("Enter a number (enter 0 to quit:) ");
+            int num = int.Parse(Console.ReadLine());
+
+            //ends to loop if 0 is entered
+            if (num == 0) {
+                break;
+            } 
+            //otherwises adds the number
+            numbers.Add(num);    
+        }
+
+        // set initial values
+        int sum = 0;
+        int largest = numbers[0];
+
+        //goes through each number and adds them together and finds the largest at the same time
+        foreach (int num in numbers) {
+            sum += num;
+            if (largest < num) {
+                largest = num;
+            }
+        }
+
+        //Finds the avarage of the numbers
+        float average = sum / numbers.Count();
+
+        //displays results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest is: {largest}");
+
     }
 }
