3343da128b33a73d249a0ef37d4ba2276d8e1b2a
JCGrant42 <kadenith@gmail.com>
2023-06-16 18:17:33 -0600

    Completed Develop05 (Shapes)

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Shape square = new Square("blue", 5);
+        Shape rectangle = new Rectangle("green", 3, 4);
+        Shape circle = new Circle("red", 4);
+        List<Shape> list = new List<Shape>();
+        list.Add(square);
+        list.Add(rectangle);
+        list.Add(circle);
+        foreach (Shape shape in list){
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Type type = shape.GetType();
+            Console.WriteLine($"This is a {color} {type} with an area of {area}");
+        }
     }
 }
---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,9 @@
+class Square : Shape{
+    private double _side;
+    public Square(string color, double side) : base(color){
+        _side = side;
+    }
+    public override double GetArea(){
+        return _side * 2;
+    }
+}
